8ceff4aa298f299ec9a4af8b639a4b495961b4dc
Marshall bonertz <32751836+marshallb007@users.noreply.github.com>
2018-06-23 10:06:32 -0400

    Add files via upload

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+    <h1 className="display-4">React Game!</h1>
+    <p className="lead">Only one Click!</p>
+  </div>
+</div>
+);
+
+export default Jumbotron;
